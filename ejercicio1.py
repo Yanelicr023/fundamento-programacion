09831b28d55f15186dd04840ed1fb5999c5ada7d
Yaneli <nelycervantes02@gmail.com>
2024-10-10 08:56:12 -0600

    01 primer ejercicio

---
+++ ejercicio1.py
@@ -0,0 +1,12 @@
+"""
+ejercicio 1:
+  escribir un programa que pregunte al usuario su nombre 
+  e imprima un saludo 
+  Estradas :
+      nombre: string
+salida: 
+   salido: string 
+   """
+nombre = input("escribe tu nombre")
+saludo = "Holaa" + nombre + "¡"
+print(saludo)
